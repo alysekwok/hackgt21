e09fd70bf848bb491ca8bf46600c665a81898a53
alysekwok <ALYSEKWOKK@GMAIL.COM>
2021-10-23 19:45:15 -0400

    finish homepage

--- client/src/components/customers/Home.js
+++ client/src/components/customers/Home.js
@@ -11,6 +11,14 @@ const Home = () => {
              <img src = {PinkBlob} alt="Pink Blob"/>
            </div>
 
+           <div id = "tagline-container">
+           <h1 class = "tagline">A home cooked meal, </h1>
+           <h1 class = "tagline">away from home</h1>
+           </div>
+
+           <div class = "start-class">
+           <button id = "create-account-button">View meals</button>
+           </div>
         </div>
     )
 }
