e531beb1a504b066997b2ada2b55aca5bab3173f
eddynmn <71167581+eddynmn@users.noreply.github.com>
2021-10-23 23:49:43 -0400

    profile design

--- client/src/components/customers/Profile.js
+++ client/src/components/customers/Profile.js
@@ -1,1 +1,26 @@
-// make page dynammic
+import React from "react";
+import './css/Profile.css';
+
+const Profile = () => {
+    return (
+        <div class ="profile-page">
+            <h3>Are you a cook or a user?</h3>
+            <h3>Info Blurb</h3>
+            <h3>My Profile</h3>
+            <div class ="profile-rectangle"></div>
+            <div class ="screen-background"></div>
+            <div class ="camera-icon"></div>
+            <div class ="user-customer-rectangle"></div>
+            <div class ="profile-box"></div>
+            <div class ="cook-user-box"></div>
+            <div class ="blurb-box"></div>
+            <div class ="buttons">
+                <button>COOK</button>
+                <button>USER</button>
+            </div>
+            <div class ="profile-image"></div>
+        </div>
+    )
+}
+
+export default Profile;
