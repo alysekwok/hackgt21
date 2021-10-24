3dfd3adfb407d0f20504181e241a9f8e193d908f
echen312 <echen312@gatech.edu>
2021-10-23 23:40:24 -0400

    rectangles yeehaw

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/customers/Home'
 import NavBar from './components/customers/NavBar'
 import Order from './components/customers/Order'
 import Pantry from './components/customers/Pantry'
+import List from './components/customers/List'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' exact component={Home} />
         <Route path='/order' component= {Order} />
         <Route path='/pantry' component= {Pantry} />
+        <Route path='/list' component= {List} />
         
       </Switch>
 
