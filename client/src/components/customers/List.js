cce9bcf8d1461c43929d7efba7055c30b422531e
echen312 <echen312@gatech.edu>
2021-10-24 00:40:31 -0400

    more list stuff

--- client/src/components/customers/List.js
+++ client/src/components/customers/List.js
@@ -5,10 +5,29 @@ const List = () => {
     return (
         <div class = "list-page" >
         <h1 class = "list-header">LIST A DISH</h1>
-        <h2 class = "img-header">TAKE A PICTURE</h2>
+        
+        
         <div class="rect"></div>
         
         <div class="img-rect"></div>
+
+        <div>
+            <h2 class="img-header">TAKE A PICTURE</h2>
+        </div>
+
+        <div class="name-rect"></div>
+        <div class="ing-rect"></div>
+        <div class="desc-rect"></div>
+
+        <div>
+            <h2 class="name-header">Dish Name</h2>
+        </div>
+        <div>
+            <h2 class="ing-header">Ingredients</h2>
+        </div>
+        <div>
+            <h2 class="desc-header">Description</h2>
+        </div>
     
         <div id = "add-button">
         <button>POST</button>
